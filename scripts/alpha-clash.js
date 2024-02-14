d0866f2e470f7e4fc5cda62595eff17df36e77c0
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-15 02:29:49 +0600

    learned about play function. but now will see how to use a function for toggle.

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,4 +1,10 @@
-function play() {
-   // Hide the home screen
-   // Show the game screen
-}
+// const homeScreen = document.getElementById("homeScreen");
+// const gameScreen = document.getElementById("gameScreen");
+
+// function play() {
+//    // Hide the home screen
+//    homeScreen.classList.add("hidden");
+
+//    // Show the game screen
+//    gameScreen.classList.remove("hidden");
+// }
