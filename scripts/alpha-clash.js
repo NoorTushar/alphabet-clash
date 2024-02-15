e0316e718c2a5613b659785ccd1e3a00c8641355
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-15 12:19:05 +0600

    random alphabet generation done

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,10 +1,49 @@
-// const homeScreen = document.getElementById("homeScreen");
-// const gameScreen = document.getElementById("gameScreen");
+function play() {
+   showElementById("gameScreen");
+   hideElementById("homeScreen");
+   continueGame();
+}
 
-// function play() {
-//    // Hide the home screen
-//    homeScreen.classList.add("hidden");
+function continueGame() {
+   // step-01 generate a random alphabet
+   let randomAlphabet = generateRandomAlphabet();
+   console.log(randomAlphabet);
+}
 
-//    // Show the game screen
-//    gameScreen.classList.remove("hidden");
-// }
+function generateRandomAlphabet() {
+   const alphabets = [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+   ];
+
+   //total 26 alphabets
+   // 0-25 index
+   let randomIndex = Math.floor(Math.random() * alphabets.length);
+
+   let randomAlphabet = alphabets[randomIndex];
+   return randomAlphabet;
+}
